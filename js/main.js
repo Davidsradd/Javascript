4cb51f5a296066a4e039bc33372649926db83a1a
Davidsradd <57205283+Davidsradd@users.noreply.github.com>
2020-01-24 18:38:25 -0600

    Javascript basics

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-alert("Welcome")
+// alert
+window.alert("Welcome");
+
+// console.log
+console.log("I hope its working");
+
+//prompt, document.write
+var name = window.prompt("How are you doing?");
+document.write("Howdy" + response + "Thats great to hear, Im doing good myself");
+
+// Operators, numerical variable
+var Dogs = 4;
+var Cats = 8;
+var animalsaddup;
+
+if(Dogs == Cats){
+animalsaddup = true
+}
+else if({
+    animalsaddup = false
+    })
+    
+document.write("animals add up:" + animalsaddup);
+
+// If statement
+var vehicle = "firetruck";
+var color = "blue";
+
+if((vehicle == "firetruck") && (color == "red")) {
+    document.write( "firetrucks are red.");
+} else{
+    document.write( "firetrucks are not that color, they're red.");
+}
+
+
+//string value
+
+var f= "George";
+var t= "Oscar";
+document.write("If" + f + "walks in, do not let" + t + "Know about it.");
+
+
